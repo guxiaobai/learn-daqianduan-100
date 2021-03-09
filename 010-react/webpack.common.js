209c1a4c7246f42a12d3eb5cc679dddd42a3c212
guxiaobai <sikuan.gu@gmail.com>
2021-03-09 16:27:56 +0800

    react

--- 010-react/webpack.common.js
+++ 010-react/webpack.common.js
@@ -2,6 +2,8 @@ const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackBar = require('webpackbar');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 
 const devMode = process.env.NODE_ENV !== 'production';
 
@@ -9,7 +11,7 @@ module.exports = {
   entry:"./src/index.js",
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: devMode ? '[name].js' : 'js/[name]-[hash].js',
+    filename: devMode ? '[name].js' : 'assets/[name]-[hash].js',
     clean: true
   },
   plugins: [
@@ -17,6 +19,9 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './public/index.html'
     }),
+    new MiniCssExtractPlugin({
+      filename: devMode ? '[name].css' : 'assets/[name]-[hash].css',
+    })
   ],
   module: {
     rules: [
@@ -24,6 +29,10 @@ module.exports = {
         test: /\.js$/i,
         exclude: /node_modules/,
         use: 'babel-loader'
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
       }
     ]
   }
