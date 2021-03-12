1fb16ac4fd8a2afe1d61d1bf12e4a1b25d9966fb
guxiaobai <sikuan.gu@gmail.com>
2021-03-12 15:39:54 +0800

    proxy

--- 010-react/config-overrides.js
+++ 010-react/config-overrides.js
@@ -1,7 +1,8 @@
 // https://github.com/arackaf/customize-cra
-// // https://github.com/timarney/react-app-rewired/
-// // https://segmentfault.com/a/1190000013220134
-// // https://github.com/iuap-design/blog/issues/260
+// https://github.com/timarney/react-app-rewired/
+// https://segmentfault.com/a/1190000013220134
+// https://github.com/iuap-design/blog/issues/260
+// https://github.com/jaywcjlove/mocker-api#using-with-create-react-app
 
 const { overrideDevServer} = require('customize-cra');
 const path = require('path');
